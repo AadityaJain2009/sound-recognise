19502c40422c1bad77c59fff5a247e2807da2862
AadityaJain2009 <81620807+AadityaJain2009@users.noreply.github.com>
2021-09-22 18:07:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+var dog = 0;
+var cat = 0;
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+
+    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
+    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+  }
+}
